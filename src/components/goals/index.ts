9a3039bfd617eea1a2c1c24385b7d758e3a0982c
sferland75 <131406899+sferland75@users.noreply.github.com>
2024-12-06 14:18:20 -0500

    Update goals index file to include GoalDashboard

--- src/components/goals/index.ts
+++ src/components/goals/index.ts
@@ -1,3 +1,4 @@
 export { default as GoalTracker } from './GoalTracker';
 export { default as GoalForm } from './GoalForm';
+export { default as GoalDashboard } from './GoalDashboard';
 export { GoalProcessor } from './GoalProcessor';
